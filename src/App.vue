72963431f253f11a3965e45866d7a9cffb909b2c
cjb <641336450@qq.com>
2023-12-17 22:54:23 +0800

    我是Jenkins自动化部署

--- src/App.vue
+++ src/App.vue
@@ -106,6 +106,7 @@ const serachServer = async(stringUrl)=> {
 
 <template>
   <div>
+    我是Jenkins自动化部署
     <el-table :data="tableData" border style="width: 100%">
       <el-table-column prop label="排名" width="80">
         <template v-slot="{row}">{{ '#'+row.rank}}</template>
